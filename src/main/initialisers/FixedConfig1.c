aa797f89607f251ad66412445faebf7809370dbf
via <via@racklap.inside.matthewvia.info>
2018-11-02 12:09:17 -0400

    New injectors and timing

--- src/main/initialisers/FixedConfig1.c
+++ src/main/initialisers/FixedConfig1.c
@@ -78,7 +78,7 @@ const volatile fixedConfig1 fixedConfigs1 FIXEDCONF1 = {
 		injectorFlow:       CC_PER_MINUTE(540),
 #elif CONFIG == VIASUPRA_ID
 		perCylinderVolume:  CYLINDER_VOLUME(500),
-		injectorFlow:       CC_PER_MINUTE(550),
+		injectorFlow:       CC_PER_MINUTE(1000),
 #else
 		perCylinderVolume:  CYLINDER_VOLUME(500),
 		injectorFlow:       CC_PER_MINUTE(550),
@@ -206,7 +206,7 @@ const volatile fixedConfig1 fixedConfigs1 FIXEDCONF1 = {
 		anglesOfTDC: {ANGLE(0), ANGLE(120), ANGLE(240), ANGLE(360), ANGLE(480), ANGLE(600), ANGLE(0), ANGLE(120), ANGLE(240), ANGLE(360), ANGLE(480), ANGLE(600)},
 		outputEventPinNumbers:       {0,1,2,0,1,2,3,4,5,3,4,5}, // Weird order because I kept messing up my wiring.
 		schedulingConfigurationBits: {0,0,0,0,0,0,1,1,1,1,1,1}, // Ones represent scheduling for injection, zeros represent scheduling for ignition
-		decoderEngineOffset:         ANGLE(182.00),
+		decoderEngineOffset:         ANGLE(10.00),
 		numberOfConfiguredOutputEvents:         12, // 6 coil events, 6 injection events
 		numberOfInjectionsPerEngineCycle:        2  // Semi-sequential
 
